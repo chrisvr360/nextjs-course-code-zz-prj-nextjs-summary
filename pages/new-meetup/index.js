0584326d78e9e24853465bf5d7afd82a6f7b6389
Chris <vr360.chris@gmail.com>
2025-05-13 17:47:44 +0200

    update still broken+

--- pages/new-meetup/index.js
+++ pages/new-meetup/index.js
@@ -1,9 +1,9 @@
 // pages/[meetupId].js
+import MeetupDetail from "../components/meetups/MeetupDetail";
 
 import { Fragment } from "react";
 import Head from "next/head";
 import { MongoClient, ObjectId } from "mongodb";
-import MeetupDetail from "../components/meetups/MeetupDetail";
 
 export default function MeetupDetailsPage({ meetup }) {
   return (
