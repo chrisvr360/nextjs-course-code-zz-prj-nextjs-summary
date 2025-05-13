1fe3bb08ca29af4e3619a6669881f3affa191e69
Chris <vr360.chris@gmail.com>
2025-05-13 17:38:39 +0200

    update still boken

--- pages/new-meetup/index.js
+++ pages/new-meetup/index.js
@@ -1,41 +1,80 @@
-// our-domain.com/new-meetup
+// pages/[meetupId].js
+
 import { Fragment } from "react";
 import Head from "next/head";
-import { useRouter } from "next/router";
+import { MongoClient, ObjectId } from "mongodb";
+import MeetupDetail from "../components/meetups/MeetupDetail";
 
-import NewMeetupForm from "../../components/meetups/NewMeetupForm";
-
-function NewMeetupPage() {
-  const router = useRouter();
-
-  async function addMeetupHandler(enteredMeetupData) {
-    const response = await fetch("/api/new-meetup", {
-      method: "POST",
-      body: JSON.stringify(enteredMeetupData),
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
-
-    const data = await response.json();
-
-    console.log(data);
-
-    router.push("/");
-  }
-
+export default function MeetupDetailsPage({ meetup }) {
   return (
     <Fragment>
       <Head>
-        <title>Add a New Meetup</title>
-        <meta
-          name="description"
-          content="Add your own meetups and create amazing networking opportunities."
-        />
+        <title>{meetup.title}</title>
+        <meta name="description" content={meetup.description} />
       </Head>
-      <NewMeetupForm onAddMeetup={addMeetupHandler} />
+      <MeetupDetail
+        image={meetup.image}
+        title={meetup.title}
+        address={meetup.address}
+        description={meetup.description}
+      />
     </Fragment>
   );
 }
 
-export default NewMeetupPage;
+export async function getStaticPaths() {
+  const user = encodeURIComponent(process.env.MONGODB_USER);
+  const password = encodeURIComponent(process.env.MONGODB_PASSWORD);
+  const cluster = process.env.MONGODB_CLUSTERNAME;
+  const dbName = process.env.MONGODB_DATABASE;
+  const uri = `mongodb+srv://${user}:${password}@${cluster}.mongodb.net/${dbName}?retryWrites=true&w=majority`;
+
+  const client = await MongoClient.connect(uri);
+  const db = client.db(dbName);
+  const meetups = await db
+    .collection("meetups")
+    .find({}, { projection: { _id: 1 } })
+    .toArray();
+  client.close();
+
+  return {
+    fallback: "blocking",
+    paths: meetups.map((m) => ({
+      params: { meetupId: m._id.toString() },
+    })),
+  };
+}
+
+export async function getStaticProps({ params }) {
+  const meetupId = params.meetupId;
+
+  const user = encodeURIComponent(process.env.MONGODB_USER);
+  const password = encodeURIComponent(process.env.MONGODB_PASSWORD);
+  const cluster = process.env.MONGODB_CLUSTERNAME;
+  const dbName = process.env.MONGODB_DATABASE;
+  const uri = `mongodb+srv://${user}:${password}@${cluster}.mongodb.net/${dbName}?retryWrites=true&w=majority`;
+
+  const client = await MongoClient.connect(uri);
+  const db = client.db(dbName);
+  const meetup = await db
+    .collection("meetups")
+    .findOne({ _id: new ObjectId(meetupId) });
+  client.close();
+
+  if (!meetup) {
+    return { notFound: true };
+  }
+
+  return {
+    props: {
+      meetup: {
+        id: meetup._id.toString(),
+        title: meetup.title,
+        address: meetup.address,
+        image: meetup.image,
+        description: meetup.description || "",
+      },
+    },
+    revalidate: 10,
+  };
+}
